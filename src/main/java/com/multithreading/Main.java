c7b959168f057caa2e97e6370b8f900370f671fa
Praveen Shrivas <praveenshrivas87@gmail.com>
2023-04-26 11:48:05 -0500

    added git-demo project to local repo

---
+++ src/main/java/com/multithreading/Main.java
@@ -0,0 +1,12 @@
+package com.multithreading;
+
+public class Main {
+
+    public void addData(String data){
+        System.out.println("Data = "+ data);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
